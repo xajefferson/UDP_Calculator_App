3948f1394480151580d5fa8d1e3f838bc4cef58a
Xavier Jefferson <xaj2012@gmail.com>
2021-05-31 03:43:36 -0500

    Calculations and commands imp but not tested

--- Calculations.java
+++ Calculations.java
@@ -1,5 +1,29 @@
 public class Calculations {
 
-    
+    public static double calcCircArea(double radius){
+        //Assumes no negative number is given
+        return Math.PI * (radius * radius);
+    }
+
+    public static double calcCircCircumference(double area){
+        return 2 * Math.PI * Math.sqrt(area/Math.PI);
+    }
+
+    public static double calcSphereVolume(double radius){
+        return (4/3) * Math.PI * (radius * radius * radius);
+    }
+
+    public static double calcSphereRadius(double area){
+        return (1/2) * Math.sqrt(area/Math.PI);
+    }
+
+    public static double calcCylinderArea(double radius, double height){
+        //TODO: Check this for order of operations
+        return 2 * Math.PI * radius * height + 2 * Math.PI * (radius * radius);
+    }
+
+    public static double calcCylinderHeight(double volume, double radius){
+        return volume / (Math.PI * (radius * radius));
+    }
     
 }
--- Commands.java
+++ Commands.java
@@ -136,26 +136,216 @@ public class Commands {
 
     public static void circle_area(User client, String[] array, Ledger l) {
 
+        //Check if circle was called first
+
+        //store arrat[1] into var and sund that var to Utility.convertStrToDouble 
+        //If it returns a -1 then invalid input was sent
+        //Check that array[1] is is a non negative number, if it is let client know then return 
+
+        //Check if client exists in ledger
+        if (!(l.socketAddrExists(client.getUserSocketAddress()))) {
+            // If the client is not in the ledger
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503,
+                   "Bad sequence of commands, send HELO before CIRCLE and AREA");
+            return;
+        }
+
+        //Check if circle was called first
+        if(!(client.isCircleTriggered())){
+            //if the client didnt call CIRCLE first
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503, "Bad sequence of commands, send CIRCLE first.");
+            return;
+        }
+        
+        String calc = array[1];
+        Double converted_result = Utility.convert_string_to_double(calc);
+
+        //Check if valid number was recieved
+        if(converted_result <= 0){
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 501, "Syntax error in parameters or arguments");
+            return;
+        }
+
+        //Valud number was recieved now send response to client
+        String to_send = String.valueOf(Calculations.calcCircArea(converted_result));
+
+        Utility.sendResponse(client.getUserIP(), client.getUserPort(), 250, to_send);
+        return;
+
     }
 
     public static void circle_circumference(User client, String[] array, Ledger l) {
+
+        //Cleck if client is in ledger
+        if (!(l.socketAddrExists(client.getUserSocketAddress()))) {
+            // If the client is not in the ledger
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503,
+                   "Bad sequence of commands, send HELO before CIRCLE and CIRC");
+            return;
+        }
+
+        //Check if circle was called first
+        if(!(client.isCircleTriggered())){
+            //if the client didnt call CIRCLE first
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503, "Bad sequence of commands, send CIRCLE first.");
+            return;
+        }
+        
+        String calc = array[1];
+        Double converted_result = Utility.convert_string_to_double(calc);
+
+        //Check if valid number was recieved
+        if(converted_result <= 0){
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 501, "Syntax error in parameters or arguments");
+            return;
+        }
+
+        //Valid number was recieved now send response to client
+        String to_send = String.valueOf(Calculations.calcCircCircumference(converted_result));
+
+        Utility.sendResponse(client.getUserIP(), client.getUserPort(), 250, to_send);
+        return;
+
+
 
     }
 
     public static void sphere_volume(User client, String[] array, Ledger l) {
 
+
+        if (!(l.socketAddrExists(client.getUserSocketAddress()))) {
+            // If the client is not in the ledger
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503,
+                   "Bad sequence of commands, send HELO before SPHERE and VOL");
+            return;
+        }
+
+        //Check if sphere was called first
+        if(!(client.isSphereTriggered())){
+            //if the client didnt call CIRCLE first
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503, "Bad sequence of commands, send SPHERE first.");
+            return;
+        }
+        
+        String calc = array[1];
+        Double converted_result = Utility.convert_string_to_double(calc);
+
+        //Check if valid number was recieved
+        if(converted_result <= 0){
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 501, "Syntax error in parameters or arguments");
+            return;
+        }
+
+        //Valid number was recieved now send response to client
+        String to_send = String.valueOf(Calculations.calcSphereVolume(converted_result));
+
+        Utility.sendResponse(client.getUserIP(), client.getUserPort(), 250, to_send);
+        return;
+
+
     }
 
     public static void sphere_radius(User client, String[] array, Ledger l) {
+        
+        if (!(l.socketAddrExists(client.getUserSocketAddress()))) {
+            // If the client is not in the ledger
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503,
+                   "Bad sequence of commands, send HELO before SPHERE and RAD");
+            return;
+        }
+
+        //Check if sphere was called first
+        if(!(client.isSphereTriggered())){
+            //if the client didnt call CIRCLE first
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503, "Bad sequence of commands, send SPHERE first.");
+            return;
+        }
+        
+        String calc = array[1];
+        Double converted_result = Utility.convert_string_to_double(calc);
+
+        //Check if valid number was recieved
+        if(converted_result <= 0){
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 501, "Syntax error in parameters or arguments");
+            return;
+        }
+
+        //Valid number was recieved now send response to client
+        String to_send = String.valueOf(Calculations.calcSphereRadius(converted_result));
+
+        Utility.sendResponse(client.getUserIP(), client.getUserPort(), 250, to_send);
+        return;
 
     }
 
     public static void cylinder_area(User client, String[] array, Ledger l) {
 
+        if (!(l.socketAddrExists(client.getUserSocketAddress()))) {
+            // If the client is not in the ledger
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503,
+                   "Bad sequence of commands, send HELO before CYLINDER and AREA");
+            return;
+        }
+
+        //Check if cylinder was called first
+        if(!(client.isCylinderTriggered())){
+            //if the client didnt call CYLINDER first
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503, "Bad sequence of commands, send CYLINDER first.");
+            return;
+        }
+        
+        String r = array[1];
+        String h = array[2];
+        Double r_converted = Utility.convert_string_to_double(r);
+        Double h_converted = Utility.convert_string_to_double(h);
+
+        
+        //Check if valid number was recieved
+        if(r_converted <= 0 || h_converted <=0){
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 501, "Syntax error in parameters or arguments");
+            return;
+        }
+
+        //Valid number was recieved now send response to client
+        String to_Send = String.valueOf(Calculations.calcCylinderArea(r_converted, h_converted));
+
+        Utility.sendResponse(client.getUserIP(), client.getUserPort(), 250, to_Send);
+        return;
     }
 
     public static void cylinder_height(User client, String[] array, Ledger l) {
 
+        if (!(l.socketAddrExists(client.getUserSocketAddress()))) {
+            // If the client is not in the ledger
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503,
+                   "Bad sequence of commands, send HELO before CYLINDER and AREA");
+            return;
+        }
+
+        //Check if cylinder was called first
+        if(!(client.isCylinderTriggered())){
+            //if the client didnt call CYLINDER first
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 503, "Bad sequence of commands, send CYLINDER first.");
+            return;
+        }
+        
+        String V = array[1];
+        String r = array[2];
+        Double V_converted = Utility.convert_string_to_double(V);
+        Double r_converted = Utility.convert_string_to_double(r);
+
+        
+        //Check if valid number was recieved
+        if(V_converted <= 0 || r_converted <=0){
+            Utility.sendResponse(client.getUserIP(), client.getUserPort(), 501, "Syntax error in parameters or arguments");
+            return;
+        }
+
+        //Valid number was recieved now send response to client
+        String to_Send = String.valueOf(Calculations.calcCylinderHeight(V_converted, r_converted));
+
+        Utility.sendResponse(client.getUserIP(), client.getUserPort(), 250, to_Send);
+        return;
     }
 
 }
